37f16c3491310557bfe09484f38588b287a52a87
ithanmang <ithanmang@163.com>
2019-07-13 17:37:55 +0800

    add GLSLLoader js file

--- home/lib/FileLoader.js
+++ home/lib/FileLoader.js
@@ -1,48 +1,98 @@
-(function () {
 
-	function FileLoader() { }
+// GLSL 文件加载 
+var GLSLLoader = function () {
 
-	Object.assign(FileLoader.prototype, {
+	// 加载单个着色器文件
+	this.load = function (url) {
 
-		load: function (url, onLoad, onError) {
+		return new Promise(function (resolve, reject) {
 
-			if (url === undefined) url = '';
+			loader(url, function (response) {
 
-			var request = new XMLHttpRequest();
+				resolve(response);
 
-			request.open('GET', url, true);
+			}, function (error) {
 
-			request.addEventListener('load', function (event) {
-
-				var responseText = this.responseText;
-
-				if (this.status === 200 || this.status === 0) {
-
-					if (this.status === 0) console.warn('HTTP 状态码 0');
-
-					// 通过正则表达式去掉注释
-					responseText = responseText.replace(/(\n)|(\/{2,}.*?(\r|\n))|(\/\*(\n|.)*?\*\/)/g, '');
-
-					onLoad(responseText);
-
-				} else {
-
-					request.onerror = function (event) {
-
-						onError(event)
-
-					}
-
-				}
+				reject(error)
 
 			})
 
-			request.send(null);
+		})
+
+	}
+
+	// 加载两个着色器文件
+	this.loadAll = function (urls, onLoad, onError) {
+
+		var shader = {};
+
+		if ((typeof urls) == 'string') {
+
+			urls = [urls];
 
 		}
 
-	})
+		urls.forEach(values => {
 
-	window.FileLoader = FileLoader;
+			scope.load(values).then(function (data) {
 
-})()
+				var fragment = data.includes('gl_FragColor');
+
+				if (fragment) {
+					shader.fragment = data;
+				} else {
+					shader.vertex = data;
+				}
+
+				if (shader.vertex && shader.fragment) onLoad(shader);
+
+			}, function (error) {
+
+				onError(error)
+
+			})
+
+		});
+
+	}
+
+	var scope = this;
+
+	function loader(url, onLoad, onError) {
+
+		if (!url) console.warn('url is error or is undefined...');
+
+		var xhr = new XMLHttpRequest();
+
+		xhr.open('GET', url, true);
+
+		xhr.addEventListener('load', function (event) {
+
+			var responseText = this.responseText;
+
+			if (this.status !== 200) {
+
+				onError(event);
+
+			} else {
+
+				// 通过正则表达式去掉注释
+				responseText = responseText.replace(/(\n)|(\/{2,}.*?(\r|\n))|(\/\*(\n|.)*?\*\/)/g, '');
+
+				onLoad(responseText);
+
+			}
+
+		}, false);
+
+		xhr.addEventListener('error', function (event) {
+
+			onError(event);
+
+		}, false);
+
+		xhr.send(null);
+
+	}
+
+}
