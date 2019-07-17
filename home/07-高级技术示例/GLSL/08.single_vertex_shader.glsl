42cdb2a9c2512eb5ad5836715551e16140093fd4
ithanmang <ithanmang@163.com>
2019-07-17 10:43:25 +0800

    modify

--- home/07-高级技术示例/GLSL/08.single_vertex_shader.glsl
+++ home/07-高级技术示例/GLSL/08.single_vertex_shader.glsl
@@ -8,7 +8,7 @@ varying vec4 v_Color;
 void main(){
 
     vec3 lightDirection = vec3(0.0, 0.0, 1.0); // 灯光的位置 在世界坐标系中
-    vec4 color = vec4(0.0, 1.0, 1.0, 1.0);// 表面颜色
+    vec4 color = vec4(0.5, 0.5, 0.8, 1.0);// 表面颜色
     gl_Position = u_MvpMatrix * a_Position; // 计算后的顶点坐标
     vec3 normal = normalize(vec3(u_NormalMatrix * a_Normal));//标准化法向量
     float nDot = max(dot(normal, lightDirection), 0.0);
